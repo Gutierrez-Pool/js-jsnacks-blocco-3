fd169248c5a38968095c407ca84eeb2247fdf16c
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-12 17:54:20 +0100

    snack-1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,28 @@
 // Snack 1:
 
 // Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+
+
+
+const buttonElement = document.getElementById("button");
+    
+const resultElement = document.getElementById("result");
+
+
+
+buttonElement.addEventListener("click", function() {
+
+    const girato = document.getElementById("parola").value;
+
+    function word () {
+    
+        let reverse = girato.split("").reverse().join("");
+    
+        console.log (reverse);
+    }
+    
+    word (girato);
+});
+
+
+
